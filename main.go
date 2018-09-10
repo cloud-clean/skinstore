35b0e31cd2e63cef9e58c6acdd750172cb6f0814
starchain <starchainstc@gmail.com>
2018-09-10 20:35:34 +0800

    update wxmsg

--- main.go
+++ main.go
@@ -2,31 +2,30 @@ package main
 
 import (
 	"net/http"
-	"time"
-	"skinstore/web/router"
-	"skinstore/web"
 	"skinstore/common/logger"
-	"skinstore/mqtt"
-	"fmt"
-	mqtt2 "github.com/eclipse/paho.mqtt.golang"
+	"skinstore/web"
+	"skinstore/web/router"
+	"strconv"
+	"time"
 )
 
 func main(){
+	port := 8082
 
 	r := &router.Router{}
 	r.RegHandlers(web.InitRoute())
 	svr :=http.Server{
-		Addr:":8082",
+		Addr:":"+strconv.Itoa(port),
 		ReadTimeout:time.Second*5,
 		WriteTimeout:time.Second*5,
 		Handler:r,
 	}
 
-	mqtt.MqttInit()
-	mqtt.Mc.Subscribe(func(client mqtt2.Client, message mqtt2.Message) {
-		fmt.Println("get msg form mqtt:"+string(message.Payload()))
-	})
-	logger.NewLog().Info("start server ...")
+	//mqtt.MqttInit()
+	//mqtt.Mc.Subscribe(func(client mqtt2.Client, message mqtt2.Message) {
+	//	fmt.Println("get msg form mqtt:"+string(message.Payload()))
+	//})
+	logger.NewLog().Infof("start server listen on:%s",svr.Addr)
 	svr.ListenAndServe()
 }
 
