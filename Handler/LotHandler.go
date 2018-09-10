a5f047dd26c52cac3ad3908618491777bef0430a
starchain <starchainstc@gmail.com>
2018-09-10 20:33:59 +0800

    update wxmsg

---
+++ Handler/LotHandler.go
@@ -0,0 +1,52 @@
+package Handler
+
+import (
+	"net/http"
+	"skinstore/Entity/lot"
+	"skinstore/common"
+	"skinstore/web/router"
+)
+
+func LampStatusHander(params *router.Params,rw http.ResponseWriter) *common.WebResult{
+	pos := params.Get("pos")
+	if pos != ""{
+		lotEntity := lot.GetLot(pos)
+
+		return common.NewResult(1,lotEntity)
+	}else{
+		return common.NewResult(0,"pos is null")
+	}
+}
+
+
+func SaveLampHander(params *router.Params,rw http.ResponseWriter) *common.WebResult{
+	pos := params.Get("pos")
+	status := params.Get("status")
+
+	if pos != "" && status != ""{
+		var lot = lot.LotEntity{Pos:pos,Status:status}
+		if lot.Save(){
+			return common.NewResult(1,"success")
+		}
+		return common.NewResult(0,"false")
+	}else{
+		return common.NewResult(0,"pos is null")
+	}
+}
+
+
+
+func UpdateLampHander(params *router.Params,rw http.ResponseWriter) *common.WebResult{
+	pos := params.Get("pos")
+	status := params.Get("status")
+
+	if pos != "" && status != ""{
+		var lot = lot.LotEntity{Pos:pos,Status:status}
+		if lot.Update(){
+			return common.NewResult(1,"success")
+		}
+		return common.NewResult(0,"false")
+	}else{
+		return common.NewResult(0,"pos is null")
+	}
+}
