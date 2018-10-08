be61de5761116ba80fdf39cf7a11e458953a008b
starchain <starchainstc@gmail.com>
2018-10-08 14:10:56 +0800

    update wxmsg

--- web/server.go
+++ web/server.go
@@ -55,6 +55,7 @@ func InitTemplate()[]router.TempRoute{
 
 func InitComm()[]router.CommRoute{
 	addCommRoute("POST","/api/lot/login",CommHandler.LotLoginHandler)
+	addCommRoute("POST","/api/lot/access",CommHandler.LotTokenAccess)
 	return commRouters;
 }
 
