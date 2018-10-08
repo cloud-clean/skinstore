1668ff23407e51ce6b3765b56ec9cc2fb61c867f
starchain <starchainstc@gmail.com>
2018-10-08 14:07:54 +0800

    update wxmsg

--- CommHandler/OauthHandler.go
+++ CommHandler/OauthHandler.go
@@ -2,6 +2,7 @@ package CommHandler
 
 import (
 	"net/http"
+	"net/url"
 	"skinstore/common/logger"
 )
 
@@ -9,11 +10,25 @@ var log = logger.NewLog()
 
 func LotLoginHandler(r *http.Request,w http.ResponseWriter){
 	r.ParseForm()
-	redirectUrl := "https://open.bot.tmall.com/oauth/callback"
 	acc := r.Form.Get("account")
 	pwd := r.Form.Get("password")
-	log.Infof("account:%s  password:%s",acc,pwd)
+
 	referUrl := r.Header.Get("referer")
-	log.Infof("referUrl:%s",referUrl)
+	decoderUrl,_ := url.QueryUnescape(referUrl)
+	referUri,_ := url.ParseRequestURI(decoderUrl)
+	refParams := referUri.Query()
+	redirectUrl := refParams.Get("redirect_uri")
+	skillId := refParams.Get("skillId")
+	clientId := refParams.Get("client_id")
+	//resType := refParams.Get("response_type")
+	log.Infof("account:%s  password:%s  skillId:%s   clientId:%s",acc,pwd,skillId,clientId)
+	redirectUrl = redirectUrl+"?code=asdfaeafaea"
 	http.Redirect(w,r,redirectUrl,http.StatusMovedPermanently)
+}
+
+func LotTokenAccess(r *http.Request,w http.ResponseWriter){
+	r.ParseForm()
+	for k,v := range r.PostForm{
+		log.Infof("key:%s  value:%s",k,v)
+	}
 }
