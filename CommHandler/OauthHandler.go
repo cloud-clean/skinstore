d3aa2f32cd7071a5bb5eb6cf7bbf7eb87f415541
starchain <starchainstc@gmail.com>
2018-10-08 13:10:42 +0800

    update wxmsg

--- CommHandler/OauthHandler.go
+++ CommHandler/OauthHandler.go
@@ -1,8 +1,19 @@
 package CommHandler
 
-import "net/http"
+import (
+	"net/http"
+	"skinstore/common/logger"
+)
+
+var log = logger.NewLog()
 
 func LotLoginHandler(r *http.Request,w http.ResponseWriter){
 	r.ParseForm()
-
+	redirectUrl := "https://open.bot.tmall.com/oauth/callback"
+	acc := r.Form.Get("account")
+	pwd := r.Form.Get("password")
+	log.Infof("account:%s  password:%s",acc,pwd)
+	referUrl := r.Header.Get("referer")
+	log.Infof("referUrl:%s",referUrl)
+	http.Redirect(w,r,redirectUrl,http.StatusMovedPermanently)
 }
