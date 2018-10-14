be67ed9f6c78d4ca946431c9441beab4df6b1ce6
cloud <amingb-518@163.com>
2018-10-14 21:37:19 +0800

    add close connection

--- Entity/lot/LotAliCallbackEntity.go
+++ Entity/lot/LotAliCallbackEntity.go
@@ -9,7 +9,22 @@ type AliCallback struct {
 		PayLoadVersion int		`json:"payLoadVersion"`
 	} 							`json:"header"`
 	Payload struct{
-		accessToken string
+		AccessToken string		`json:"accessToken"`
+		Devices []LotDevice	`json:"devices"`
+		Actions []string		`json:"actions"`
+		Extension map[string]string `json:"extension"`
 	}							`json:"payload"`
 
+}
+
+type LotDevice struct {
+	DeviceId string 	`json:"deviceId"`
+	DeviceName string 	`json:"deviceName"`
+	DeviceType string 	`json:"deviceType"`
+	Zone 		string	`json:"zone"`
+	Model		string 	`json:"model"`
+	Icon 		string  `json:"icon"`
+	Value 		string 	`json:"value"`
+	Attribute	string 	`json:"attribute"`
+	Properties []map[string]string 	`json:"properties"`
 }
