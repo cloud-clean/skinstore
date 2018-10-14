dc21c83c8e1abfffc85bd46578671b4534e5db1f
cloud <amingb-518@163.com>
2018-10-14 23:11:03 +0800

    add close connection

--- CommHandler/OauthHandler.go
+++ CommHandler/OauthHandler.go
@@ -76,7 +76,7 @@ func LotCallback(r *http.Request,w http.ResponseWriter){
 			resp.Header.Name = "DiscoveryDeviceResponse"
 			resp.Header.MessageId = callParam.Header.MessageId
 			resp.Header.PayLoadVersion = 1
-			devices := []lot.LotDevice{lot.LotDevice{DeviceId:"cloud_lot_1",DeviceName:"cloudLot",DeviceType:"lot",Model:"lot",Icon:"https://www.home-assistant.io/demo/favicon-192x192.png"}}
+			devices := []lot.LotDevice{lot.LotDevice{DeviceId:"cloud_lot_1",DeviceName:"cloudLot",DeviceType:"lot",Model:"lot",Brand:"cloud",Icon:"https://www.home-assistant.io/demo/favicon-192x192.png"}}
 			resp.Payload.Devices = devices
 			resp.Payload.Actions = []string{"TrunOn,TrunOff"}
 			b,err := json.Marshal(resp)
